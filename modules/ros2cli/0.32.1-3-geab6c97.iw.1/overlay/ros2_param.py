f3c5f8e3a3b1f46c14112bb54b2382bde204f416
Eddy Gharib <eddy.gharib@idealworks.com>
2024-12-13 15:34:49 +0100

    [ros2cli] Added wrapping scripts for lifecycle, node and param commands and removed dep on rules_ros2

---
+++ modules/ros2cli/0.32.1-3-geab6c97.iw.1/overlay/ros2_param.py
@@ -0,0 +1,37 @@
+# Copyright 2022 Milan Vukov
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import sys
+
+import ros2_cmd
+import ros2cli.cli
+import ros2param.verb.delete
+import ros2param.verb.describe
+import ros2param.verb.dump
+import ros2param.verb.get
+import ros2param.verb.list
+import ros2param.verb.load
+import ros2param.verb.set
+
+COMMAND_EXTENSIONS = {
+    "delete": ros2param.verb.delete.DeleteVerb(),
+    "describe": ros2param.verb.describe.DescribeVerb(),
+    "dump": ros2param.verb.dump.DumpVerb(),
+    "get": ros2param.verb.get.GetVerb(),
+    "list": ros2param.verb.list.ListVerb(),
+    "load": ros2param.verb.load.LoadVerb(),
+    "set": ros2param.verb.set.SetVerb(),
+}
+
+extension = ros2_cmd.Ros2CommandExtension(COMMAND_EXTENSIONS)
+sys.exit(ros2cli.cli.main(extension=extension))
