735c40decaecdede1acb08083b93115f211362f5
Eddy Gharib <eddy.gharib@idealworks.com>
2025-01-20 07:59:04 +0100

    [aws-crt-cpp] Removed unmatched glob calls

---
+++ modules/aws-crt-cpp/0.27.3.iw.3/overlay/include/aws/crt/Config.h
@@ -0,0 +1,11 @@
+#pragma once
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#define AWS_CRT_CPP_VERSION "0.27.3"
+#define AWS_CRT_CPP_VERSION_MAJOR 0
+#define AWS_CRT_CPP_VERSION_MINOR 27
+#define AWS_CRT_CPP_VERSION_PATCH 3
+#define AWS_CRT_CPP_GIT_HASH "17e30acb7d6338a7b180c0cd522de0b4c51ceadd"
