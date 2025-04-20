5efae500fb96c741a9d2591242a81336f14a3f06
Eddy Gharib <eddy.gharib@idealworks.com>
2025-04-20 23:43:30 +0200

    [aws-crt-cpp] Added v0.32.4

---
+++ modules/aws-crt-cpp/0.32.4.iw.1/overlay/include/aws/crt/Config.h
@@ -0,0 +1,11 @@
+#pragma once
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#define AWS_CRT_CPP_VERSION "0.32.4"
+#define AWS_CRT_CPP_VERSION_MAJOR 0
+#define AWS_CRT_CPP_VERSION_MINOR 32
+#define AWS_CRT_CPP_VERSION_PATCH 4
+#define AWS_CRT_CPP_GIT_HASH "f8437df651af0795e024f02a944bd464319220ee"
